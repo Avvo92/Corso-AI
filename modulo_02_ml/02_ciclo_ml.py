c877c44393cd2bc3903e4b2c6039f1874a85b9ae
Salvatore Galasso <gianlucagiamminonni1992@gmail.com>
2026-04-02 10:06:04 +0200

    svolto esercizio n7 capitolo 2 modulo 2

--- modulo_02_ml/02_ciclo_ml.py
+++ modulo_02_ml/02_ciclo_ml.py
@@ -809,6 +809,7 @@ print("\nEsercizio 4")
 import pandas as pd
 import numpy as np
 import os
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
@@ -818,7 +819,6 @@ case = pd.read_csv(path_case_file)
 X = case[['metri_quadri','anno_costruzione']]
 y = case['prezzo_euro']
 
-from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 modello = DecisionTreeRegressor(random_state=42, max_depth=5)
@@ -859,7 +859,7 @@ print(f"R² Test  =>{r2_test:.3f}")
 # modello = DecisionTreeRegressor(max_depth=4)
 # modello.fit(X_train, y_train)
 #
-# Suggerimento: il Decision Tree di Scikit-Learn accetta feature categoriche?
+# Il problema di questo codice è che il modello DecisionTreeRegressor non accetta opzioni categoriche tra le feature. Prima di procedere con la fase di addestramento e predizione occorre fare one-hot encoding per la categoria citta.
 #
 
 # ESERCIZIO 6 (🔀 [INTERLEAVING] — Pandas + ML):
@@ -872,7 +872,63 @@ print(f"R² Test  =>{r2_test:.3f}")
 # 5) Stampa MAE e R² e confronta con la baseline
 # 6) Scrivi in 2 righe: allenarti su una sola città è una buona idea?
 #    Perché sì o perché no?
-#
+
+print("\nEsercizio 6\n")
+case['prezzo_medio_mq'] = (case['prezzo_euro'] / case['metri_quadri']).round(2)
+report = case.groupby("citta").agg(
+    prezzo_medio = ('prezzo_euro', lambda x: x.mean().round(2)),
+    pratiche_totali = ('id', 'count'),
+    prezzo_medio_mq = ('prezzo_medio_mq', lambda x: x.mean().round(2))
+)
+
+top_citta = ((report.sort_values(by='prezzo_medio_mq', ascending=False).head(1)).index)[0]
+mask = case['citta'] == top_citta
+
+filtered_case = case.loc[mask]
+
+cols_to_drop = ['prezzo_euro', 'prezzo_medio_mq', 'citta']
+
+X = filtered_case.drop(columns=cols_to_drop)
+y= filtered_case['prezzo_euro']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=.2, random_state=42
+)
+
+modello = DecisionTreeRegressor(max_depth=5, random_state=42)
+modello.fit(X_train, y_train)
+
+y_pred_baseline = np.full_like(y_test, y_train.mean())
+y_pred_train = modello.predict(X_train)
+y_pred_test = modello.predict(X_test)
+
+mae_baseline = mean_absolute_error(y_test, y_pred_baseline)
+r2_baseline = r2_score(y_test, y_pred_baseline)
+
+mae_train = mean_absolute_error(y_train, y_pred_train)
+r2_train = r2_score(y_train, y_pred_train)
+
+mae_test = mean_absolute_error(y_test, y_pred_test)
+r2_test = r2_score(y_test, y_pred_test)
+
+
+print(f"Reali Train      => {y_train.values.astype(int)}")
+print(f"Previsioni Train => {y_pred_train.astype(int)}")
+print(f"MAE Train        => {mae_train:.2f}")
+print(f"R² Train         => {r2_train:.3f}\n")
+
+print(f"Reali Test      => {y_test.values.astype(int)}")
+print(f"Previsioni Test => {y_pred_test.astype(int)}")
+print(f"MAE Test        => {mae_test:.2f}")
+print(f"R² Test         => {r2_test:.3f}\n")
+
+print(f"Reali Test     => {y_test.values.astype(int)}")
+print(f"Prev. Baseline => {y_pred_baseline.astype(int)}")
+print(f"MAE Baseline   => {mae_baseline:.2f}")
+print(f"R² Baseline    => {r2_baseline:.3f}\n")
+
+
+#in conclusione, dalle comparazioni effettuate, si evince che il modello così come configurato non possiede grande capacità previsionale. Questo non è dovuto in particolare alla scelta di focalizzarsi solamente alla citta con prezzi più alti (nel nostro caso Milano), ma al fatto che, così facendo, il dataset a disposizione per train e test si è molto ridotto. Questa mancanza di dati non ha permesso al modello di avere sufficiente casistica dalla quale poter evincere le regole generali della determinazione del prezzo. Ne consegue che se il dataset, anche se circoscritto alla sola Milano, se fosse di dimensioni adeguate potrebbe a quel punto aiutarci a allenare adeguatamente un modello previsionale efficace nella determinazione dei prezzi della città.
 
 # ESERCIZIO 7 (🧠 [RETRIEVAL] — riscrittura da memoria):
 # Senza guardare il codice sopra, riscrivi da zero il ciclo completo:
@@ -884,7 +940,64 @@ print(f"R² Test  =>{r2_test:.3f}")
 # 6) Calcola baseline
 # 7) Calcola e confronta metriche (MAE, R²) training vs test vs baseline
 # 8) Scrivi un assert
-#
+
+print("\nEsercizio 7\n")
+#carico il file file "case.csv" e lo leggo come Dataframe Pandas
+path_file_case = os.path.join(os.path.dirname(__file__), "dati", "case.csv")
+case = pd.read_csv(path_file_case)
+
+#Creo 2 nuove feature per descrivere in maniera più efficace i record del DataFrame case.
+case['log_mq'] = np.log1p(case['metri_quadri'])
+case['mq_per_stanze'] = case['metri_quadri'] / case['num_stanze'].replace(0, np.nan)
+
+#Faccio one-hot encoding su case perchè il decision tree non legge feature categoriche
+case_encoded = pd.get_dummies(case, columns=['citta'], dtype='int')
+
+#Preparo le features e il target in modo da assicurarmi di evitare il leakage
+cols_to_drop = (['prezzo_euro'] + [c for c in case_encoded.columns if ('prezzo' in c) or ('fascia' in c)])
+X = case_encoded.drop(columns=cols_to_drop, errors='ignore')
+y = case_encoded['prezzo_euro']
+
+#Eseguo lo split del DataFrame in train e test
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=.2, random_state=42
+)
+
+#assegno il modello alla variabile "modello",imposto gli iperparametri e lo addestro
+modello = DecisionTreeRegressor(max_depth=6, random_state=42)
+modello.fit(X_train, y_train)
+y_pred_train = modello.predict(X_train)
+y_pred_test = modello.predict(X_test)
+
+#calcolo la baseline
+y_baseline = np.full_like(y_test, y_train.mean())
+
+#imposto le metriche per effettuare i confronti
+mae_baseline = mean_absolute_error(y_test, y_baseline)
+r2_baseline = r2_score(y_test, y_baseline)
+
+mae_train = mean_absolute_error(y_train, y_pred_train)
+r2_train = r2_score(y_train, y_pred_train)
+
+mae_test = mean_absolute_error(y_test, y_pred_test)
+r2_test = r2_score(y_test, y_pred_test)
+
+print(f"Reali Train      => {y_train[:5].values.astype(int)}")
+print(f"Previsioni Train => {y_pred_train[:5].astype(int)}")
+print(f"MAE Train        => {mae_train:.2f}")
+print(f"R² Train         => {r2_train:.3f}\n")
+
+print(f"Reali Test      => {y_test[:5].values.astype(int)}")
+print(f"Previsioni Test => {y_pred_test[:5].astype(int)}")
+print(f"MAE Test        => {mae_test:.2f}")
+print(f"R² Test         => {r2_test:.3f}\n")
+
+print(f"Reali Test     => {y_test.values.astype(int)}")
+print(f"Prev. Baseline => {y_baseline.astype(int)}")
+print(f"MAE Baseline   => {mae_baseline:.2f}")
+print(f"R² Baseline    => {r2_baseline:.3f}\n")
+
+assert mae_test < mae_baseline, "il modello deve battere la baseline"
 
 # ESERCIZIO 8 (🔀 [INTERLEAVING] — Confronto modelli):
 # Allena 3 Decision Tree con max_depth diversi: 2, 5, 10.
